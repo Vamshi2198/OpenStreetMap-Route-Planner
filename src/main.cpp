fe803319739138a35431d8faff569526467749ee
Vamshi_Krishna <vamshikrishna.t21@gmail.com>
2021-06-30 14:50:36 -0400

    Declare start and end coordinates to accept user input

--- src/main.cpp
+++ src/main.cpp
@@ -51,10 +51,12 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+    // user input for start and end coordinates
+    float start_x, start_y, end_x, end_y;
+    std::cin >> start_x ;
+    std::cin >> start_y ;
+    std::cin >> end_x;
+    std::cin >> end_y ; 
 
     // Build Model.
     RouteModel model{osm_data};
